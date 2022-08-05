01eb38f37d208607bd3594175001bab47dc8d1e6
Jason <jasonliu215@gmail.com>
2022-08-05 22:45:21 +1200

    Accidently removed auth.js

---
+++ server/routes/note.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const Note = require('../models/Note');
+
+const noteRouter = express.Router();
+
+noteRouter.get('/note', async (req, res) => {
+    const note = await Note.findById(req.noteId);
+    res.json({...user._doc, token: req.token})
+}) 
+
+
+module.exports = noteRouter
